d397fe4a0ddf91c02ec4720e2658832c2d716adc
David Shan <davidshan20070302@gmail.com>
2023-09-16 17:07:19 -0400

    pull

--- pages/index.js
+++ pages/index.js
@@ -121,7 +121,6 @@ export default function Home() {
                                     }
                                     className="border border-gray-300 rounded-md p-2 block"
                                 />
-                                <p>Result: {res}</p>
                                 <button
                                     onClick={handleUpload}
                                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
