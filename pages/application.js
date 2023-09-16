8a2d80de021576f8ffda2dd5df4d47165db94177
Léo Liu <leoliuhy@gmail.com>
2023-09-16 18:23:19 -0400

    merged homepage to main

--- pages/application.js
+++ pages/application.js
@@ -1,133 +1,112 @@
 import { useState } from "react";
 
-export default function Application() {
+export default function Home() {
     const [file, setFile] = useState(null);
     const [res, setRes] = useState("None");
     const [loading, setLoading] = useState(false);
-    const [data, setData] = useState(""); // Initialize data1 state
+    const [text, setText] = useState("milk");
+    const [script, setScript] = useState("");
+    const [result, setResult] = useState("");
 
-    async function onSubmit(e) {
-        e.preventDefault();
-
-        if (!file) {
-            setRes("Please upload a file");
-            return;
-        }
-        setLoading(true);
+    const handleUpload = async () => {
         try {
+            if (!file) {
+                throw new Error("Please upload a file");
+            }
+            setLoading(true);
             const formData = new FormData();
             formData.append("pdf", file);
-
-            const response = await fetch("/api/uploadpdf", {
+            const uploadResponse = await fetch("/api/uploadpdf", {
                 method: "POST",
                 body: formData,
             });
-
-            if (response.status === 200) {
-                setRes("PDF uploaded and saved successfully");
-            } else {
+            if (uploadResponse.status !== 200) {
                 throw new Error(
-                    `API call failed with status ${response.status}`
+                    `API call failed with status ${uploadResponse.status}`
                 );
             }
-        } catch (err) {
-            console.error(err);
-            setRes(err.message);
-        } finally {
-            setLoading(false);
-        }
-
-        setFile(e.target.files?.[0]);
-        const formData = new FormData();
-        let dataRes;
-        try {
-            setLoading(true);
-
-            formData.append("pdf", file);
-            console.log(formData);
-            const response = await fetch("/api/pdfparse", {
+            const pdfParseResponse = await fetch("/api/pdfparse", {
                 method: "POST",
             });
-            if (response.status === 200) {
-                dataRes = await response.json();
-                //console.log("API response:", dataRes.txt);
-                //setData(dataRes.txt);
-            } else {
+            if (pdfParseResponse.status !== 200) {
                 throw new Error(
-                    `API call failed with status ${response.status}`
+                    `API call to pdfparse failed with status ${pdfParseResponse.status}`
                 );
             }
-            console.log("THIS IS THE TEXT:", dataRes.txt); 
-            setRes("Success");
-            setLoading(false);
-        } catch (err) {
-            console.log(err);
-            setRes(err.message);
-        }
-        try {
-            setLoading(true);
-            console.log("BRRRRR", dataRes.txt); 
-            const response = await fetch("/api/openAI", {
+            const pdfParseData = await pdfParseResponse.json();
+            const updatedText = pdfParseData.txt;
+            setText(updatedText);
+
+            const generateResponse = await fetch("/api/generate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ textInput: updatedText }),
+            });
+
+            if (generateResponse.status !== 200) {
+                throw new Error(
+                    `API call to generate failed with status ${generateResponse.status}`
+                );
+            }
+
+            const generateData = await generateResponse.json();
+            setResult(generateData);
+            const audioResponse = await fetch("/api/synthesizeAudio", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                    input: dataRes.txt,
+                    script: generateData,
+                    fileName: "output.mp3",
                 }),
             });
-
-            if (!response.ok) {
-                throw new Error("Failed to fetch data from OpenAI");
-            }
-            
-            const data = await response.json();
-            console.log(data);
+            const audio = new Audio("/audio/output.mp3");
+            audio.addEventListener("loadedmetadata", () => {
+                const durationInSeconds = audio.duration;
+                audio.play();
+            });
             setRes("Success");
+        } catch (error) {
+            console.error(error);
+            setRes(error.message);
+        } finally {
             setLoading(false);
-        } catch (err) {
-            console.error(err);
-            setRes(err.message);
-            setLoading(false);
+            console.log("Finished!");
         }
-    }
+    };
 
     return (
-        <>
-            <div className="flex flex-col items-center justify-center min-h-screen py-2">
-                <div className="flex flex-col items-center gap-4">
-                    {loading ? (
-                        <div>loading</div>
-                    ) : (
-                        <>
-                            <h1 className="text-6xl font-bold ">
-                                PDF to video
-                            </h1>
-                            <p>Upload the pdf of a video file here</p>
-                            <form
-                                onSubmit={(e) => onSubmit(e)}
-                                className="flex flex-col items-center gap-2"
-                            >
-                                <input
-                                    type="file"
-                                    name="file"
-                                    onChange={(e) =>
-                                        setFile(e.target.files?.[0])
-                                    }
-                                    className="border border-gray-300 rounded-md p-2 block"
-                                />
-                                <p>Result: {res}</p>
-                                <button
-                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                    type="submit"
-                                >
-                                    Upload
-                                </button>
-                            </form>
-                        </>
-                    )}
+        <div className="flex flex-col items-center gap-4 justify-center min-h-screen py-2">
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <div>
+                    <h1 className="text-6xl font-bold">PDF Uploader</h1>
+                    <p>Upload a PDF file here</p>
+                    <form
+                        onSubmit={(e) => e.preventDefault()}
+                        encType="multipart/form-data"
+                    >
+                        <input
+                            type="file"
+                            name="pdf"
+                            accept=".pdf"
+                            onChange={(e) => setFile(e.target.files?.[0])}
+                            className="border border-gray-300 rounded-md p-2 block"
+                        />
+                        <button
+                            onClick={handleUpload}
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            type="button"
+                        >
+                            Upload
+                        </button>
+                    </form>
                 </div>
-            </div>
-        </>
+            )}
+        </div>
     );
 }
