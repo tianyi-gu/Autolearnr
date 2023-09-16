d7d8ea6f0b7d0ff26c14e0689c333f3f1f9b71b3
David Shan <davidshan20070302@gmail.com>
2023-09-16 18:08:26 -0400

    fixed hydration error

--- pages/index.js
+++ pages/index.js
@@ -34,15 +34,15 @@ export default function Home() {
                 );
             }
             const pdfParseData = await pdfParseResponse.json();
-            const updatedText = pdfParseData.txt; 
-            setText(updatedText); 
+            const updatedText = pdfParseData.txt;
+            setText(updatedText);
 
             const generateResponse = await fetch("/api/generate", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ textInput: updatedText }), 
+                body: JSON.stringify({ textInput: updatedText }),
             });
 
             if (generateResponse.status !== 200) {
@@ -58,66 +58,55 @@ export default function Home() {
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ script: generateData, fileName: "output.mp3" }),
+                body: JSON.stringify({
+                    script: generateData,
+                    fileName: "output.mp3",
+                }),
             });
             const audio = new Audio("/audio/output.mp3");
             audio.addEventListener("loadedmetadata", () => {
                 const durationInSeconds = audio.duration;
                 audio.play();
             });
-            if (audioResponse.status === 200) {
-                const audioData = await audioResponse.json();
-                setResult("Audio generated successfully");
-            } else {
-                throw new Error(
-                    `API call to synthesizeAudio failed with status ${audioResponse.status}`
-                );
-            }
             setRes("Success");
         } catch (error) {
             console.error(error);
             setRes(error.message);
         } finally {
             setLoading(false);
-            console.log("Finished!")
+            console.log("Finished!");
         }
     };
 
     return (
-        <>
-            <div className="flex flex-col items-center justify-center min-h-screen py-2">
-                <div className="flex flex-col items-center gap-4">
-                    {loading ? (
-                        <div>Loading...</div>
-                    ) : (
-                        <>
-                            <h1 className="text-6xl font-bold">PDF Uploader</h1>
-                            <p>Upload a PDF file here</p>
-                            <form
-                                onSubmit={(e) => e.preventDefault()}
-                                encType="multipart/form-data"
-                            >
-                                <input
-                                    type="file"
-                                    name="pdf"
-                                    accept=".pdf"
-                                    onChange={(e) =>
-                                        setFile(e.target.files?.[0])
-                                    }
-                                    className="border border-gray-300 rounded-md p-2 block"
-                                />
-                                <button
-                                    onClick={handleUpload}
-                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                    type="button"
-                                >
-                                    Upload
-                                </button>
-                            </form>
-                        </>
-                    )}
+        <div className="flex flex-col items-center gap-4 justify-center min-h-screen py-2">
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <div>
+                    <h1 className="text-6xl font-bold">PDF Uploader</h1>
+                    <p>Upload a PDF file here</p>
+                    <form
+                        onSubmit={(e) => e.preventDefault()}
+                        encType="multipart/form-data"
+                    >
+                        <input
+                            type="file"
+                            name="pdf"
+                            accept=".pdf"
+                            onChange={(e) => setFile(e.target.files?.[0])}
+                            className="border border-gray-300 rounded-md p-2 block"
+                        />
+                        <button
+                            onClick={handleUpload}
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            type="button"
+                        >
+                            Upload
+                        </button>
+                    </form>
                 </div>
-            </div>
-        </>
+            )}
+        </div>
     );
 }
