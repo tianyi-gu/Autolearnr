0086cb972736961be15c0baef65388610646a09d
Roshan Hegde <roshanhegde@outlook.com>
2023-09-16 18:43:20 -0400

    typo

--- pages/api/openAI.js
+++ pages/api/openAI.js
@@ -76,7 +76,7 @@ export default async function handler(req, res) {
 
         // Split script into chunks based on main points
       response = await openai.chat.completions.create({
-        temperature: 0
+        temperature: 0,
         model: "gpt-3.5-turbo-16k",
         messages: [
           {
