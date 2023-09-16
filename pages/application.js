e90bee6680a51f10dd258fe5972da58125439df5
Roshan Hegde <roshanhegde@outlook.com>
2023-09-16 04:26:19 -0400

    Homepage

---
+++ pages/application.js
@@ -0,0 +1,59 @@
+import button from '../components/ui/button'
+import { useState } from 'react'
+
+export default function Application() {
+  const [file, setFile] = useState(null)
+  const [res, setRes] = useState("None")
+  const [loading, setLoading] = useState(false)
+  
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    setFile(e.target.files?.[0])
+
+    const formData = new FormData()
+    try {
+      if (!file) {
+        throw new Error('Please upload a file')
+      }
+      setLoading(true)
+      formData.append('file', file)
+      console.log(file)
+      setRes("Success")
+      setLoading(false)
+    }
+    catch (err) {
+      console.log(err)
+      setRes(err.message)
+    }
+  }
+
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        <div className='flex flex-col items-center gap-4'>
+          {loading ? (
+            <div>loading</div>
+          ) : ( 
+          <>
+          <h1 className='text-6xl font-bold '>PDF to video</h1>
+          <p >Upload the pdf of a video file here</p>
+          <form onSubmit={(e) => onSubmit(e)} className='flex flex-col items-center gap-2'>
+            <input
+              type="file"
+              name="file"
+              onChange={(e) => setFile(e.target.files?.[0])}
+              className="border border-gray-300 rounded-md p-2 block"
+            />
+            <p>Result: {res}</p>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' type='submit'>
+            Upload
+          </button>
+          </form>
+          </>
+          ) }
+        </div>
+      </div>
+    </>
+  )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,14 @@
-import button from '../components/ui/button'
-import { useState } from 'react'
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  const [file, setFile] = useState(null)
-  const [res, setRes] = useState("None")
-  const [loading, setLoading] = useState(false)
-  
-  const onSubmit = async (e) => {
-    e.preventDefault()
-    setFile(e.target.files?.[0])
-
-    const formData = new FormData()
-    try {
-      if (!file) {
-        throw new Error('Please upload a file')
-      }
-      setLoading(true)
-      formData.append('file', file)
-      console.log(file)
-      setRes("Success")
-      setLoading(false)
-    }
-    catch (err) {
-      console.log(err)
-      setRes(err.message)
-    }
-  }
-
+  const router = useRouter();
   return (
-    <>
-      <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-        <div className='flex flex-col items-center gap-4 '>
-          {loading ? (
-            <div>loading</div>
-          ) : ( 
-          <>
-          <h1 className='text-6xl font-bold '>PDF to video</h1>
-          <p >Upload the pdf of a video file here</p>
-          <form onSubmit={(e) => onSubmit(e)}>
-            <input
-              type="file"
-              name="file"
-              onChange={(e) => setFile(e.target.files?.[0])}
-              className="border border-gray-300 rounded-md p-2 block"
-            />
-            <p>Result: {res}</p>
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' type='submit'>
-            Upload
-          </button>
-          </form>
-          </>
-          ) }
-        </div>
-      </div>
-    </>
-  )
+    <div className="flex flex-col items-center justify-center gap-4 min-h-screen">
+      <h1 className="text-4xl font-bold">Review slides effortlessly</h1>
+      <p className="max-w-200px"> Our solution generates a Khan Academy style video using any pdf (such as your lecture notes, or slides), allowing you to easily review or learn content.</p>
+      <Button onClick={() => {router.push("/application")}}>Start</Button>
+    </div>
+  );
 }
-
